d7aec99b916e646c62ba7b6ab544a92f0f1be1b4
Jay <seojoongwon2020@gmail.com>
2025-05-23 00:05:43 +0900

    필요없는 제거

--- first_try_11720_rust/target/debug/deps/first_try_11720_rust-2c0ef21766862893.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Jungle\Documents\wk12\BOJ\first_try_11720_rust\target\debug\deps\libfirst_try_11720_rust-2c0ef21766862893.rmeta: src\main.rs
-
-C:\Users\Jungle\Documents\wk12\BOJ\first_try_11720_rust\target\debug\deps\first_try_11720_rust-2c0ef21766862893.d: src\main.rs
-
-src\main.rs:
--- first_try_11720_rust/target/debug/first_try_11720_rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Jungle\Documents\wk12\BOJ\first_try_11720_rust\target\debug\first_try_11720_rust.exe: C:\Users\Jungle\Documents\wk12\BOJ\first_try_11720_rust\src\main.rs
